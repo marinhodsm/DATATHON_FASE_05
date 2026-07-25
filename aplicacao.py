a119a8a3aea99bd4057291e7d23cf1af2d5edc78
marinhodsm <diegosassomarinho@gmail.com>
2026-07-25 16:59:44 -0300

    Add files via upload

--- aplicacao.py
+++ aplicacao.py
@@ -1,21 +1,2 @@
-# Aplicação Streamlit — Dashboard + Predição de Obesidade
-
-# ==================================================
-# BIBLIOTECAS
-# ==================================================
-
-import joblib
-import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# ==================================================
-# CONFIGURAÇÃO DA PÁGINA
-# ==================================================
-
-st.set_page_config(
-    page_title='TESTE',
-    page_icon='icone.png',
-    layout='wide'
-)
+st.title("TESTE")

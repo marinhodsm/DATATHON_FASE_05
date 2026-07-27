0680bfa64453e46c9dce430d9275db4741698e83
marinhodsm <diegosassomarinho@gmail.com>
2026-07-27 20:46:21 -0300

    Add files via upload

--- aplicacao.py
+++ aplicacao.py
@@ -53,7 +53,7 @@ st.markdown('---')
 # CARREGAMENTO DO MODELO TREINADO
 # ==================================================
 
-modelo = joblib.load('models/modelo_previsao_defasagem.pkl')
+modelo = joblib.load('C:/Users/diego/Desktop/RM368720_DATATHON_FASE_05/models/modelo_previsao_defasagem.pkl')
 
 # ==================================================
 # ABAS PARA NAVEGAÇÃO DE PÁGINA
